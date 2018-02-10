403dc0ce320efd9eedacd71fcfcf1a021e915fb3
Cris <crisgalasanchez@gmail.com>
2018-02-10 15:26:22 +0100

    edit profile

--- src/components/User.js
+++ src/components/User.js
@@ -5,9 +5,16 @@ class User extends Component {
 	render() {
 		return (
 			<div className="App">
-				
+				<div class="profile">
+					<div class="wrapper">
+						<div class="profile--avatar">
+							<img src="https://cdn.pixabay.com/photo/2013/07/12/12/18/user-145538_960_720.png" alt="name" class="profile--avatar-image"/>
+						</div>
+						<h1 class="profile--name">Nombre y apellido</h1>
+						<h2 class="profile--role">Username</h2>
+					</div>
+				</div>
 				<SocialNetwork />
-
 			</div>
 		);
 	}
