53715d175ddaec5064cfb6ca0797f2b5b7cfddaf
Mamen <mamenrueco@gmail.com>
2018-02-12 11:15:35 +0100

    styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,26 +12,47 @@ class Header extends Component {
 		return (
 					<header className="header">
 						<div className="header--icon">
-							<a href="#home"><img src={icon} className="header--icon--logo" alt="icon-header--menu" /></a>
+							<a href="http://bitbloq.bq.com/#/">
+								<img src={icon} className="header--icon--logo" alt="icon-header--menu" />
+							</a>
 						</div>
 						<nav className="header--menu">
 							<ul className="header--menu--list">
-								<li className="header--menu--link">Proyectos</li>
-								<li className="header--menu--link">Explora</li>
 								<li className="header--menu--link">
-									<img src={iconStudents} className="menu--link-icon" />
-									<span className="menu--link--span">Usuarios</span>
+									<a className="menu--link menu--link-projects" href="">
+
+										<span className="menu--link--span">Proyectos</span>
+									</a>
+								</li>
+								<li className="header--menu--link menu--link-explore">
+									<a className="menu--link" href="">
+										<img src={iconStudents} className="menu--link-icon" />
+										<span className="menu--link--span">Explora</span>
+									</a>
 								</li>
 								<li className="header--menu--link">
-									<img src={iconStory} className="menu--link-icon" />
-									<span className="menu--link--span">Aprende</span>
+									<a className="menu--link" href="">
+										<img src={iconStudents} className="menu--link-icon menu--link-users" />
+										<span className="menu--link--span">Usuarios</span>
+									</a>
 								</li>
-								<li className="header--menu--link">Ayuda</li>
+								<li className="header--menu--link">
+									<a className="menu--link" href="">
+										<img src={iconStory} className="menu--link-icon menu--link-learn" />
+										<span className="menu--link--span">Aprende</span>
+									</a>
+								</li>
+								<li className="header--menu--link">
+									<a className="menu--link" href="">
+										<img src={iconStory} className="menu--link-icon menu--link-forum" />
+										<span className="menu--link--span">Foro</span>
+									</a>
+								</li>
 							</ul>
 						</nav>
 						<div className="menu--button">
 							<button className="header--menu--button">Entrar</button>
-						</div>
+							</div>
 					</header>
 				);
 				}
