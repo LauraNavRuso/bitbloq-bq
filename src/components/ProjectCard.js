e3660949ec720205216f6355141324ddb1af0b1b
LauraNavRuso <laura.nav.ruso@gmail.com>
2018-02-15 13:05:48 +0100

    merge api-petition to dev

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,27 +3,26 @@ import placa1 from './images/placa1.jpg';
 
 class ProjectCard extends Component {
 	render() {
+		let baseImgUrl = `https://storage.googleapis.com/bitbloq/images/project/`;
+		let projectImgUrl = baseImgUrl + this.props.idProject;
+
 		return (
 			<div className="App">
-
 				<div className="grid--project-card">
 					<div className="App-header">
-						<img className="App-title" src={placa1}/>
+						<img className="App-title" src={projectImgUrl}/>
 					</div>
 
 					<div className="App-intro">
-						
-
-						<p>{this.props.name}</p>
-
-						{/* <p>{this.props.projectList}</p> */}
-
+						<p>Usuario: {this.props.username}</p>
+						<p>Nombre del proyecto: {this.props.name}</p>
+						<p>Veces descargado: {this.props.timesAdded}</p>
+						<p>Veces visto: {this.props.timesViewed}</p>
+						<p>Id proyecto: {this.props.idProject}</p>
 					</div>
 				</div>
-
 			</div>
-				);
+		);
 	}
 }
-
 export default ProjectCard;
