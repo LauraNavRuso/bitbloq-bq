79bc6ca15c753b7a5a8ce563876ddef517a409fc
Gabriela Mena <gabriela.grojas@gmail.com>
2018-02-20 13:41:05 +0100

    try styles in sort elements

---
+++ src/components/actionsBar/Sort.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Sort extends Component {
+	render() {
+		return (
+			<div>
+				<select className="order--button">
+					<option className="order--option" value='' disabled>Orden:</option>
+					<option className="order--option" value='recientes'>Más recientes</option>
+					<option className="order--option" value='antiguos'>Más antiguos</option>
+					<option className="order--option" value='a-z'>Nombre A-Z</option>
+					<option className="order--option" value='z-a'>Nombre Z-A</option>
+					<option className="order--option" value='votados'>Más votados</option>
+				</select>
+			</div>
+		);
+	}
+}
+
+export default Sort;
