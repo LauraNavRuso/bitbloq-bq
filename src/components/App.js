a62a171ca05c694eb2bb1737084b86dee14e9f3a
Gabriela Mena <gabriela.grojas@gmail.com>
2018-02-22 09:46:44 +0100

    add a specific user to stay static firt time in page

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,18 @@ import PaginationBar from './PaginationBar';
 
 class App extends Component {
 	constructor(props){
-			super(props);
-			this.requestServer = this.requestServer.bind(this);
-			this.handleInput = this.handleInput.bind(this);
-			this.state = {
-				projects: [],
-				creator:{_id:'5a8dc42409d5f4001b7fdea6',
-							username:'gamer'},
-				projectsForSpecificUser: []
+		super(props);
+		this.requestServer = this.requestServer.bind(this);
+		this.handleInput = this.handleInput.bind(this);
+		this.state = {
+			projectsForSpecificUser: []
 			};
 		}
 
 	componentDidMount() {
 		let objectUserInputs = {
 			creator: {
-				_id: `546e259ce4b0bde006d07afe`
+				_id: '5a8dc42409d5f4001b7fdea6'
 			}
 		}
 
@@ -31,40 +28,22 @@ class App extends Component {
 			});
 		}
 
-		this.requestServer(`https://api-beta-bitbloq.bq.com/bitbloq/v1/project?`,
+		this.requestServer('http://api-next.bitbloq.k8s.bq.com/bitbloq/v1/project?',
 			objectUserInputs, successFn);
 	}
 
 	requestServer(baseApiUrl, objectUserInputs, callbackFn) {
-		let apiPagination = `page=0`;
-		console.log(apiPagination);
 		let creatorData = JSON.stringify(objectUserInputs);
-		let apiEndpoint = baseApiUrl + apiPagination + `&query=` + creatorData;
+		let apiEndpoint = baseApiUrl + '&query=' + creatorData;
 
 		fetch(apiEndpoint)
 		.then(response => response.json())
 		.then(json => {
 			callbackFn(json);
-			console.log(json);
 		})
 		.catch(function(error){
-			console.log('Ha sucedido un error: ' + error);
-		}
-	)
-
+		})
 	}
-
-//Get data from API with promises
-	// componentDidMount() {
-	// 	fetch('http://api-next.bitbloq.k8s.bq.com/bitbloq/v1/project?page=0')
-	// 	.then(response => response.json())
-	// 	.then(json => {
-	// 		this.setState({
-	// 			projects: json
-	// 		});
-	// 		// console.log('Estoy imprimiendo:', json);
-	// 	});
-	// }
 
 	//Get data from input
 	handleInput(event){
@@ -78,6 +57,7 @@ class App extends Component {
 		            "$options":"i"
 		         }
 		      },
+
 		      {
 		         "creator":{
 		            "$regex":searchValue,
@@ -94,39 +74,28 @@ class App extends Component {
 			});
 		}
 
-		this.requestServer(`http://api-next.bitbloq.k8s.bq.com/bitbloq/v1/project?`,
+		this.requestServer('http://api-next.bitbloq.k8s.bq.com/bitbloq/v1/project?',
 			filterQuery, successFn);
 	}
-
-
 
 //'http://api-next.bitbloq.k8s.bq.com/bitbloq/v1/project?page=0&query={"$or":[{"name":{"$regex":"coche","$options":"i"}},{"creator":{"$regex":"coche","$options":"i"}}]}
 //'
 
-
-render() {
-
-	// let project = this.state.projects;
-	// 	//Condición para filtar por letra introducida
-	// 	if(this.state.filter){
-	// 		characters = characters.filter( character => character.name.toLowerCase().includes(this.state.filter.toLowerCase()))
-	// 	}
-
+	render() {
 		return (
-				<div className="page">
-					<div className="nav">
-						<Header />
-						<User projects={this.state.projectsForSpecificUser} />
-					</div>
-					<div className="main">
-
-						<ActionsBar handleInput={this.handleInput} />
-						{this.state.projectsForSpecificUser.map(x =>(
-							<ProjectCard idProject={x._id} name={x.name} username={x.creator.username}  timesAdded={x.timesAdded} timesViewed={x.timesViewed} />
-						))}
-						<PaginationBar />
-					</div>
+			<div className="page">
+				<div className="nav">
+					<Header />
+					<User projects={this.state.projectsForSpecificUser} />
 				</div>
+				<div className="main">
+					<ActionsBar handleInput={this.handleInput} />
+						{this.state.projectsForSpecificUser.map(x =>(
+					<ProjectCard idProject={x._id} name={x.name} username={x.creator.username}  timesAdded={x.timesAdded} timesViewed={x.timesViewed} />
+						))}
+					<PaginationBar />
+				</div>
+			</div>
 		);
 	}
 }
