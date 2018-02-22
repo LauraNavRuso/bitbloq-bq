6d0a9b76da6c0b6184924d03a294320e870bcaef
Mamen <mamenrueco@gmail.com>
2018-02-22 11:43:44 +0100

    function pagination

--- src/components/PaginationBar.js
+++ src/components/PaginationBar.js
@@ -1,62 +1,80 @@
 import React, { Component } from 'react';
 
 class PaginationBar extends Component {
-	// constructor(props) {
-	// 	super(props);
-	// 	this.updatePagination=this.updatePagination.bind(this);
-	// 	this.totalPages=this.totalPages.bind(this);
-	// 	this.state = {
-	// 		totalProjectsUser: 0,
-	// 		totalPagesUser: 0
-	// 	}
+	constructor(props) {
+		super(props);
+		this.updatePagination=this.updatePagination.bind(this);
+		this.state = {
+			projectsInfoUpdate: false,
+			totalProjectsUser: 0,
+			totalPagesUser: 0,
+			pageProjects: []
+		}
+}
+	componentDidMount() {
+		this.updatePagination(0);
+	}
+	updatePagination(page){
+		console.log(page);
+
+		const url = `https://api-beta-bitbloq.bq.com/bitbloq/v1/project?page=${page}&query=%7B%22creator%22:%7B%22_id%22:%22546e259ce4b0bde006d07afe%22%7D%7D`;
+
+		if ((page === 0) && (this.state.projectsInfoUpdate === false)) {
+			// llamamos al count
+			const urlCount = url + '&count=%2A';
+			fetch(urlCount)
+			.then(response => response.json())
+			.then(json =>{
+				const totalProjects = json.count;
+				let totalPages = Math.ceil(totalProjects/20);
+				if (totalPages === 0) totalPages = 1
+				this.setState({
+					totalProjectsUser: totalProjects,
+					totalPagesUser: totalPages,
+					projectsInfoUpdate: true
+				});
+			});
+
+		}
+
+		// Llamada de los página de proyectos que toque
+		fetch(url)
+		.then(response => response.json())
+		.then(json =>{
+			this.setState({
+				pageProjects: json
+			});
+
+		});
+
+	}
 
 
-// 	componentDidMount() {
-// 		this.updatePagination(0);
-// 	}
-// 	updatePagination(page){
-// 		let url = `https://api-beta-bitbloq.bq.com/bitbloq/v1/project?page=${page}&query=%7B%22creator%22:%7B%22_id%22:%22546e259ce4b0bde006d07afe%22%7D%7D`;
-// 		if (page === 0) {
-// 			url = url + '&count=%2A'
-// 		}
-// 		fetch(url);
-// 		.then(response => response.json())
-// 		.then(json =>{
-// 			if (page===0) {
-// 			console.log(json.count);
-// 			const totalProjects = json.count;
-// 			const totalPages = Math.ceil(totalProjects/20);
-// 			this.setState({
-// 				totalProjectsUser: totalProjects,
-// 				totalPagesUser: totalPages
-// 			});
-// 		}
-// 		});
-//
-// 	}
-//
-//
-// totalPages(){
-// 	let total = this.state.totalProjectsUser;
-// 	let totalPage = total/20;
-// 	let pageUser = Math.ceil(totalPage);
-// 	console.log(pageUser);
-//
-// }
+paintPagination(){
+	let buttons = [];
+	for (let i = 1; i<= this.state.totalPagesUser; i++) {
+		// console.log(this.state.totalPagesUser);
+		buttons.push (<button className="button-page">{i}</button>);
+	if (this.state.totalPagesUser === 1){
+		<button className="pagination-page" type="button" disabled>Anterior</button>
+	}
 
-	render() {
-		return (
-			<div className="pagination">
-				<button className="pagination-page">Anterior</button>
-				<button className="button-page" onClick={this.totalPages}>1</button>
-				<button className="button-page">2</button>
-				<button className="button-page">3</button>
-				<button className="button-page">4</button>
-				<button className="button-page">5</button>
-				<button className="pagination-page">Siguiente</button>
-			</div>
+	}
+	return(
+		<div className="pagination">
+			<button className="pagination-page" type="button">Anterior</button>
+			{buttons}
+			<button className="pagination-page" type="button">Siguiente</button>
+		</div>
+	)
+}
+
+			render() {
+				return (
+					<div>{this.paintPagination()}</div>
 		);
 	}
 }
+
 
 export default PaginationBar;
