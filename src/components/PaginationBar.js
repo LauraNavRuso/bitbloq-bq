662b1ff2eb8a97c9adc61c908c19f27f5ee588be
Mamen <mamenrueco@gmail.com>
2018-02-24 18:56:30 +0100

    funcionality buttons pagination test

--- src/components/PaginationBar.js
+++ src/components/PaginationBar.js
@@ -18,18 +18,6 @@ class PaginationBar extends React.Component {
 	}
 	updatePagination(page){
 
-		// https://api-next.bitbloq.k8s.bq.com/bitbloq/v1/project?page=${page}&query=%7B%22creator%22:%7B%22_id%22:%22546e259ce4b0bde006d07afe%22%7D%7D
-
-		// let url = `http://api-next.bitbloq.k8s.bq.com/bitbloq/v1/project?page=${page}`+ this.props.userId;
-
-		// let url = 'http://api-next.bitbloq.k8s.bq.com/bitbloq/v1/project?' + 'count=*&query={%22creator%22:{%22_id%22:%22' + this.props.userId + '%22}}'
-
-		//const url = `http://api-next.bitbloq.k8s.bq.com/bitbloq/v1/project?page` + this.props.userId;
-
-		//const url = 'http://api-next.bitbloq.k8s.bq.com/bitbloq/v1/page/'+ this.props.userId;
-
-		//const url = `https://api-next.bitbloq.k8s.bq.com/bitbloq/v1/project?page=${page}` + this.props.userId;
-
 		let baseApi = 'https://api-beta-bitbloq.bq.com/bitbloq/v1/project?';
 		let countApi = 'count=*&query={%22creator%22:{%22_id%22:%22';
 		let closeApi = '%22}}';
@@ -37,12 +25,12 @@ class PaginationBar extends React.Component {
 
 		if ((page === 0) && (this.state.projectsInfoUpdate === false)) {
 			// llamamos al count
-			let urlCount = apiPageUser + '&count=%2A';
-			fetch(urlCount)
+			fetch(apiPageUser)
 			.then(response => response.json())
 			.then(json =>{
 				const totalProjects = json.count;
 				let totalPages = Math.ceil(totalProjects/20);
+				console.log('Tenemos las páginas ' + page);
 				if (totalPages === 0) totalPages = 1
 				this.setState({
 					totalProjectsUsers: totalProjects,
@@ -53,7 +41,7 @@ class PaginationBar extends React.Component {
 
 		}
 
-		// Llamada de la página de proyectos que toque
+		// Llamada a la página de proyectos que toque
 		fetch(apiPageUser)
 		.then(response => response.json())
 		.then(json =>{
@@ -62,19 +50,14 @@ class PaginationBar extends React.Component {
 				pageProjects: json,
 				actualPage: parseInt(page) +1
 			});
-
 		});
-
 	}
 
 updateActualPage(e){
-	console.log('>>>>')
 	const nextPage =parseInt(e.target.getAttribute('data-page'));
-
 console.log('> página actual: ' + this.state.actualPage);
 console.log('> página next: ' + nextPage);
 	if (nextPage != this.state.actualPage) {
-		console.log('HAGO LA PETICIÓN, A lA LOCURA!');
 		this.updatePagination(nextPage);
 	}
 }
@@ -95,8 +78,8 @@ paintPagination(){
 
 	}
 
+	//Dependiendo de las páginas
 	for (let i = 1; i<= this.state.totalPagesUser; i++) {
-		// console.log('Primer console', this.state.totalPagesUser);
 		if ( this.state.totalPagesUser === 1) {
 			buttons.push (<button className="button-page" disabled>{i}</button>);
 		} else {
@@ -123,14 +106,12 @@ paintPagination(){
 		</div>
 	)
 }
-
-			render() {
-				return (
-					<div>{this.paintPagination()}</div>
+		render() {
+			return (
+				<div>{this.paintPagination()}</div>
 		);
 	}
 
 }
-
 
 export default PaginationBar;
