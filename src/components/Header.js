255486ac4d13c3b4fac4c6ac436491e021eeea8b
Mamen <mamenrueco@gmail.com>
2018-02-24 14:23:29 +0100

    indent and static imagen

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,47 +11,46 @@ class Header extends Component {
 
 	render() {
 		return (
-					<header className="header">
-						<div className="header--icon">
-							<a href="http://bitbloq.bq.com/#/">
-								<img src={icon} className="header--icon--logo" alt="icon-header--menu" />
+			<header className="header">
+				<div className="header--icon">
+					<a href="http://bitbloq.bq.com/#/">
+						<img src={icon} className="header--icon--logo" alt="icon-header--menu" />
+					</a>
+				</div>
+				<nav className="header--menu">
+					<ul className="header--menu--list">
+						<li className="header--menu--link">
+							<a className="menu--link-projects" href="http://bitbloq.bq.com/#/bloqsproject" target="_blank">
+								Proyectos
 							</a>
-						</div>
-						<nav className="header--menu">
-							<ul className="header--menu--list">
-								<li className="header--menu--link">
-									<a className="menu--link-projects" href="http://bitbloq.bq.com/#/bloqsproject" target="_blank">
-										Proyectos
-									</a>
-								</li>
-								<li className="header--menu--link">
-									<a className="menu--link-explore" href="http://bitbloq.bq.com/#/explore" target="_blank">
-										Explora
-									</a>
-								</li>
-								<li className="header--menu--link">
-									<a className="menu--link-users" href="">
-										Usuarios
-									</a>
-								</li>
-								<li className="header--menu--link">
-									<a className="menu--link-learn" href="http://bitbloq.bq.com/#/learn" target="_blank">
-										Aprende
-									</a>
-								</li>
-								<li className="header--menu--link">
-									<a className="menu--link-forum" href="http://bitbloq.bq.com/#/forum" target="_blank">
-										Foro
-									</a>
-								</li>
-							</ul>
-						</nav>
-						<div className="menu--image">
-							<img className="header--menu-image" src={iconUser} />
-						</div>
-					</header>
-				);
-				}
-				}
-
-					export default Header;
+						</li>
+						<li className="header--menu--link">
+							<a className="menu--link-explore" href="http://bitbloq.bq.com/#/explore" target="_blank">
+								Explora
+							</a>
+						</li>
+						<li className="header--menu--link">
+							<a className="menu--link-users" href="">
+								Usuarios
+							</a>
+						</li>
+						<li className="header--menu--link">
+							<a className="menu--link-learn" href="http://bitbloq.bq.com/#/learn" target="_blank">
+								Aprende
+							</a>
+						</li>
+						<li className="header--menu--link">
+							<a className="menu--link-forum" href="http://bitbloq.bq.com/#/forum" target="_blank">
+								Foro
+							</a>
+						</li>
+					</ul>
+				</nav>
+				<div className="menu--image">
+					<img className="header--menu-image" src={iconUser} />
+				</div>
+			</header>
+);
+}
+}
+export default Header;
