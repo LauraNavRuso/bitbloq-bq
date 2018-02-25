ef8d8d8a3476f8e61826a1847a832e5fbe95895c
LauraNavRuso <laura.nav.ruso@gmail.com>
2018-02-25 20:49:39 +0100

    bcg img cover

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,13 +5,24 @@ class ProjectCard extends Component {
 	render() {
 		let baseImgUrl = `http://storage.googleapis.com/bitbloq-next/images/project/`;
 		let projectImgUrl = baseImgUrl + this.props.idProject;
+		// let projectImgUrl =	'http://via.placeholder.com/350x150';
+
+		const divStyleEachImg = {
+			backgroundImage: `url('${projectImgUrl}' )`,
+			backgroundSize: 'cover'
+		};
 
 		return (
 			<div className="projects--project-card-container">
-				<div className="project-card--container-img">
-					<img className="project-card--img" src={projectImgUrl}/>
+				<div className="project-card--container-img"
+					
+					style={divStyleEachImg}>
 
 				</div>
+
+				<div>
+
+					</div>
 
 				<div className="project-card--container-info">
 					<h3 className="project-card--main-title">{this.props.name}</h3>
@@ -26,9 +37,7 @@ class ProjectCard extends Component {
 							<div className="project-card--timesDownloaded">
 								{this.props.timesDownloaded}
 							</div>
-
 						</div>
-
 					</div>
 				</div>
 			</div>
@@ -36,3 +45,17 @@ class ProjectCard extends Component {
 	}
 }
 export default ProjectCard;
+
+			{/* <img className="project-card--img" src={projectImgUrl}/> */}
+
+					// style={{backgroundImage: "url(" + Background + ")"}}
+
+					// background: 'url('+ imgUrl + ') noRepeat center center fixed',
+					// backgroundSize: 'cover',
+
+				// background: `url( ${projectImgUrl})`,
+
+
+			{/* <img src="http://via.placeholder.com/350x100" /> */}
+
+						// background: `url( ${projectImgUrl})`,
